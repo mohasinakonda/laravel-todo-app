c9fdd100f4bdd3795decd4d16ba58b629d028c3b
mohasinakonda <mohasinakonda39@gmail.com>
2023-07-23 13:48:49 +0600

    feat: add regions

---
+++ database/migrations/2023_07_23_074444_create_regions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('country_id');
+            $table->string('region_name');
+            $table->timestamps();
+            $table->foreign('country_id')
+                ->references('id')->on('countries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('regions');
+    }
+};
