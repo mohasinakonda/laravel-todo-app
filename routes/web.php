f6feda628a0f8d74ec669956334c688e55e5d4b9
mohasinakonda <mohasinakonda39@gmail.com>
2023-11-13 22:33:35 +0600

    chore: show task with eloquent modal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskController2;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::post('/create-todo', [TaskController::class, 'store']);
+Route::put('/todo/{id}', [TaskController2::class, 'update']);
+Route::get('/dashboard', function () {
+    $todos = Task::latest()->orderBy('created_at', 'desc')->get();
+    return view('dashboard', ['todos' => $todos]);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::view('/greeting', 'greeting');
+Route::get('tasks', function () {
+    return view('practice', [
+        'tasks' => Task::latest()->orderBy('status', 'desc')->get()
+    ]);
+})->name('tasks.index');
+Route::get('task/{id}', function ($id) {
+    $task = \App\Models\Task::findOrFail($id)->first();
+    // if (!$task) {
+    //     return abort(\Illuminate\Http\Response::HTTP_NOT_FOUND);
+    // }
+    return view('show', [
+        'task' => $task
+    ]);
+})->name('task.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
