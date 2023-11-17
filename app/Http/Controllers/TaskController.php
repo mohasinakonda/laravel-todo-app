6db0aae8b60b589a97aeff6dbfb28b01db061964
mohasinakonda <mohasinakonda39@gmail.com>
2023-11-17 22:08:25 +0600

    chore:task is validate

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,15 +17,21 @@ class TaskController extends Controller
     }
     public function store(Request $request)
     {
-        $task = Task::create($request->all());
-        return response()->json([
-            'message' => 'success',
+        $data = $request->validate([
+            "name" => "required|max:255|unique:tasks",
+            "description" => 'required',
+            "long_description" => 'required'
         ]);
+        $task = new Task;
+        $task->name = $data['name'];
+        $task->description = $data['description'];
+        $task->long_description = $data['long_description'];
+        $task->save();
+        return redirect()->route('task.show', ['id' => $task->id]);
     }
 
     public function update(Task $task, Request $request)
     {
-        dd('taskid' . $task);
         $task->status = $request->status;
         $task->name = $request->name;
         $task->description = $request->description;
