00554d13d36f819f35d7b3749fed6f1a56ec73f6
mohasinakonda <mohasinakonda39@gmail.com>
2023-11-18 22:21:55 +0600

    chore: keep old values in input field

--- resources/views/components/todo-form.blade.php
+++ resources/views/components/todo-form.blade.php
@@ -1,23 +1,24 @@
 <x-app-layout>
-    <h2 class="text-3xl text-center">lists</h2>
+    <h2 class="text-3xl text-center">Add Task</h2>
     <form method="POST" action="{{ url('create-todo') }}" class="max-w-lg mx-auto">
         <div class="flex flex-col gap-5">
             <div class="w-full py-2">
 
-                <input name="name" type="text" placeholder="Task name" class="w-full px-5 rounded focus:ring-0">
+                <input name="name" value="{{ old('name') }}" type="text" placeholder="Task name"
+                    class="w-full px-5 rounded focus:ring-0">
                 @error('name')
                     <small class="text-red-400">{{ $message }}</small>
                 @enderror
             </div>
             <div class="w-full ">
-                <textarea placeholder="Task description" name="description" class="w-full rounded resize-none focus:ring-0"></textarea>
+                <textarea placeholder="Task description" name="description" class="w-full rounded resize-none focus:ring-0">{{ old('description') }}</textarea>
                 @error('description')
                     <small class="text-red-400">{{ $message }}</small>
                 @enderror
             </div>
             <div class="w-full">
 
-                <textarea placeholder=" long Task description" name="long_description" class="w-full rounded resize-none focus:ring-0"></textarea>
+                <textarea placeholder=" long Task description" name="long_description" class="w-full rounded resize-none focus:ring-0">{{ old('long_description') }}</textarea>
                 @error('long_description')
                     <small class="text-red-400">{{ $message }}</small>
                 @enderror
