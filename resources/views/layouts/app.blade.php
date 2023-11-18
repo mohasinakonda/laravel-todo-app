508be1f7df695475fdd92b0a6b92e8723aec039b
mohasinakonda <mohasinakonda39@gmail.com>
2023-11-18 20:48:48 +0600

    chore: blade app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100">
+    {{ $slot }}
+</body>
+
+</html>
